ce7d8298683b5a991c7cf2a5ac3daab74f42a9f8
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-25 16:51:53 +0200

    Add files via upload

---
+++ Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercising
+{
+    public class Dog:Animal
+    {
+        public Dog() : base() { }
+        public Dog(string name,int age,double height,bool hasFur) : base(name, age, height, hasFur) { }
+        public override void ProduceSound()
+        {
+            Console.WriteLine("Waff");
+        }
+    }
+}
