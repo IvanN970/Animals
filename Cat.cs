433d06a32964856b1a121e7106cb92d0fc1918af
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-25 16:53:57 +0200

    Add files via upload

---
+++ Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Exercising
+{
+    class Cat:Animal
+    {
+        public Cat() : base() { }
+        public Cat(string name,int age,double height,bool hasFur) : base(name, age, height, hasFur) { }
+        public override void ProduceSound()
+        {
+            Console.WriteLine("Muay");
+        }
+    }
+}
