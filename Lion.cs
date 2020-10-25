7d9b8364815065793c79d8f0767bb9380542091c
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-25 16:54:55 +0200

    Add files via upload

---
+++ Lion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Exercising
+{
+    public class Lion:Animal
+    {
+        public Lion() : base() { }
+        public Lion(string name,int age,double height,bool hasFur) : base(name, age, height, hasFur) { }
+        public override void ProduceSound()
+        {
+            Console.WriteLine("Roar");
+        }
+    }
+}
